3d9a3bef46fa7236d106ad46bfd82905d37aa010
LJS1120142097 <bingjie0825@163.com>
2016-09-19 15:19:38 +0800

    LJS-注释已加

--- mini-db/table.h
+++ mini-db/table.h
@@ -25,6 +25,8 @@ class Table
 public:
 	/**
 	*  \brief 构造函数
+	*
+	*  \param new_path 数据库存储路径
 	*/
 	Table(std::string new_path);
 
@@ -35,61 +37,101 @@ public:
 
 	/**
 	*  \brief 创建表单
+	*
+	*  \param sql SQL有效信息类
+	*
+	*  \return 创建成功返回true，否则返回false
 	*/
 	bool CreateTable(SQLCreateTable &sql);
 
 	/**
 	*  \brief 获取表单名称
+	*
+	*  \return 当前表的名称
 	*/
 	std::string GetTableName();
 
 	/**
 	*  \brief 获取表头信息
+	*
+	*  \return 当前表头存储的vector
 	*/
 	std::vector<Field> GetTableFields();
 
 	/**
 	*  \brief 重置表名
+	*
+	*  \param new_name 新的表名
 	*/
 	void SetTableName(std::string new_name);
 
 	/**
 	*  \brief 打开表单
+	*
+	*  \return 打开成功返回true,否则返回false
 	*/
 	bool UseTable();
 
 	/**
 	*  \brief 增加记录
+	*
+	*  \param st SQL语句信息类
+	*
+	*  \return 插入成功返回true,否则返回false
 	*/
 	bool CreateRecord(SQLInsert &st);
 
 	/**
 	*  \brief 更新记录
+	*
+	*  \param su SQL语句信息类
+	*
+	*  \return 更新成功返回true,否则返回false
 	*/
 	bool UpdateRecord(SQLUpdate &su);
 
 	/**
 	*  \brief 删除记录
+	*
+	*  \param sd SQL语句信息类
+	*
+	*  \return 删除成功返回true,否则返回false
 	*/
 	bool DeleteRecord(SQLDelete &sd);
 
 	/**
 	*  \brief 通过SQL直接查找记录
+	*
+	*  \param sql SQL语句信息类
+	*
+	*  \return 查找成功返回true,否则返回false
 	*/
 	bool SelectRecord(SQLSelect &sql);
 
 	/**
 	*  \brief Delete调用Select功能
+	*
+	*  \param sd SQL语句信息类
+	*
+	*  \return 查找成功返回true,否则返回false
 	*/
 	bool SelectRecord(SQLDelete &sd);
 
 	/**
 	*  \brief Update调用Select功能
+	*
+	*  \param su SQL语句信息类
+	*
+	*  \return 查找成功返回true,否则返回false
 	*/
 	bool SelectRecord(SQLUpdate &su);
 
 	/**
 	*  \brief 创建索引
+	*
+	*  \param si SQL语句信息类
+	*
+	*  \return 创建成功返回true,否则返回false
 	*/
 	bool CreateIndex(SQLCreateIndex &si);
 
@@ -100,17 +142,33 @@ public:
 
 	/**
 	*  \brief 记录显示
+	*
+	*  \param id 要显示的记录id
+	*  \param iter 输出的顺序编号
+	*
+	*  \return 显示成功返回true，否则false
 	*/
 	bool Display(USER_INT id,USER_INT iter);
 
 	/**
 	*  \brief 顺序查找
+	*
+	*  \param select_field 要查找的字段名
+	*  \param select_value 要查找的数据信息
+	*  \param select_op 对应的运算符
+	*
+	*  \return 查找成功返回true，否则false
 	*/
 	bool OrderSelect(string select_field, Value select_value, OperatorType select_op);
+
 private:
 
 	/**
 	*  \brief 找到字段对应的索引编号
+	*
+	*  \param index_field_name 索引对应的字段名
+	*
+	*  \return 查找到的索引对应编号，若没有索引返回-1
 	*/
 	int FindIndex(std::string index_field_name);
 
